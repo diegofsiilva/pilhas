25620e2720f4d666aaec9ed48b91e990562873f3
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2026-03-29 11:59:53 -0300

    feat: impletação de inversão

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,37 @@
-public class Main {
+public class Main{
     static class Pilha{
-         char[] arr;
-         int topo;
+        int topo;
+        char[] arr;
 
-         Pilha(int tamanho) {
-             arr=new char[tamanho];
-             topo=-1;
+        Pilha(int tamanho){
+            topo=-1;
+            arr=new char[tamanho];
         }
         void push(char c){
-             arr[++topo]=c;
+            arr[++topo]=c;
+        }
+        char pop(){
+             return arr[topo--];
+        }
+        boolean isEmpty(){
+            return topo ==-1;
+        }
+    }
+    public static String inverter(String s) {
+        Pilha p = new Pilha(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            p.push(s.charAt(i));
+        }
+        String res = "";
+        while (!p.isEmpty()) {
+            res += p.pop();
 
         }
-
-        public static void main(String[] args){
-
-        }
-
+        return res;
+    }
+    public static void main(String[]args){
+        String entrada="abc";
+        System.out.println(inverter(entrada));
     }
 }
